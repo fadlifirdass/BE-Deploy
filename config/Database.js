4d0cf083a04bc0c9aba7d1e60c56e948a229edd0
fadlifrl <fadlifirdass@gmail.com>
2023-03-25 06:37:28 +0700

    host changed

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require('sequelize')
 
 const db = new Sequelize("dummy99","root","",{
-    host : "localhost",
+    host : "mysql-117511-0.cloudclusters.net",
     dialect : "mysql"
 })
 
