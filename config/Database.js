ecb9f6c5129a9b3d0855151f28e00defceb92bb0
fadlifrl <fadlifirdass@gmail.com>
2023-03-25 07:00:29 +0700

    using new password for db

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require('sequelize')
 
-const db = new Sequelize("dummy99","admin","yiC40ge2",{
+const db = new Sequelize("dummy99","admin","salmantraore",{
     host : "mysql-117511-0.cloudclusters.net",
     dialect : "mysql"
 })
