23480fca5cfd9db46de73bd76b16a700771490ab
fadlifrl <fadlifirdass@gmail.com>
2023-03-25 07:23:34 +0700

    add port

--- config/Database.js
+++ config/Database.js
@@ -2,7 +2,8 @@ const {Sequelize} = require('sequelize')
 
 const db = new Sequelize("dummy99","admin","salmantraore",{
     host : "mysql-117511-0.cloudclusters.net",
-    dialect : "mysql"
+    dialect : "mysql",
+    port : 10031
 })
 
 module.exports = db;
